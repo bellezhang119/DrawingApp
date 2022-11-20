eb5d32ea1a30c78de1fdbcaf12dea8eda01c94e2
BZ <zhangzishan119@gmail.com>
2022-11-21 01:29:54 +1300

    Added saving image to file
    +Added saving image to android file

--- app/src/main/java/com/kotlinapps/drawingapp/MainActivity.kt
+++ app/src/main/java/com/kotlinapps/drawingapp/MainActivity.kt
@@ -6,16 +6,31 @@ import android.app.Dialog
 import android.content.DialogInterface
 import android.content.Intent
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.provider.Settings
+import android.view.View
 import android.widget.*
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -38,6 +53,8 @@ class MainActivity : AppCompatActivity() {
 
     private var undoButton : ImageButton? = null
     private var redoButton : ImageButton? = null
+
+    private var diskButton : ImageButton? = null
 
     private val openGalleryLauncher : ActivityResultLauncher<Intent> = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()) {
@@ -108,6 +125,8 @@ class MainActivity : AppCompatActivity() {
         undoButton = findViewById(R.id.ib_undo)
         redoButton = findViewById(R.id.ib_redo)
 
+        diskButton = findViewById(R.id.ib_disk)
+
         brushButton?.setOnClickListener {
             showBrushSizeDialog()
         }
@@ -126,6 +145,16 @@ class MainActivity : AppCompatActivity() {
 
         redoButton?.setOnClickListener {
             drawingView?.onClickRedo()
+        }
+
+        diskButton?.setOnClickListener {
+            if (isReadStorageAllowed()) {
+                lifecycleScope.launch {
+                    val flDrawingView : FrameLayout = findViewById(R.id.fl_drawing_view_container)
+
+                    saveBitmapFile(getBitmap(flDrawingView))
+                }
+            }
         }
     }
 
@@ -153,6 +182,74 @@ class MainActivity : AppCompatActivity() {
         colorPickerDialog?.show()
     }
 
+    private fun getBitmap(view : View) : Bitmap {
+        val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        val background = view.background
+
+        if (background != null) {
+            background.draw(canvas)
+        } else {
+            canvas.drawColor(Color.WHITE)
+        }
+
+        view.draw(canvas)
+
+        return bitmap
+    }
+
+    private suspend fun saveBitmapFile(bitmap : Bitmap?) : String {
+        var result = ""
+
+        withContext(Dispatchers.IO) {
+            if (bitmap != null) {
+                try {
+                    val bytes = ByteArrayOutputStream()
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 90, bytes)
+                    
+                    val file = File(externalCacheDir?.absoluteFile.toString() 
+                            + File.separator + "DrawingApp_" + System.currentTimeMillis()/1000
+                            + ".png")
+
+                    val fileOutput = FileOutputStream(file)
+                    fileOutput.write(bytes.toByteArray())
+                    fileOutput.close()
+
+                    result = file.absolutePath
+
+                    runOnUiThread {
+                        if (result.isNotEmpty()) {
+                            Toast.makeText(this@MainActivity, "File saved successfully: $result",
+                            Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(this@MainActivity, "Something went wrong",
+                                Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } catch (e : Exception) {
+                    result = ""
+                    e.printStackTrace()
+                }
+            }
+        }
+
+        return result
+    }
+
+    private fun isReadStorageAllowed() : Boolean {
+        val result = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun isManageStorageAllowed() : Boolean {
+        val result = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.MANAGE_EXTERNAL_STORAGE)
+
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
     private fun requestStoragePermission() {
         println(ActivityCompat.shouldShowRequestPermissionRationale(
             this,
@@ -164,7 +261,8 @@ class MainActivity : AppCompatActivity() {
                 "Please give Drawing App permission to access your files and media in the settings")
 
         } else {
-            requestPermission.launch(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE))
+            requestPermission.launch(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.MANAGE_EXTERNAL_STORAGE))
         }
     }
 
